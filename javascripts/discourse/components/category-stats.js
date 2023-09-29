f4493bd0a4d8eea98f6170bd79bd9a829d04fc32
Rhababo <Wesley.Adams@rhababo.com>
2023-09-28 21:24:25 -0500

    update map.size

--- javascripts/discourse/components/category-stats.js
+++ javascripts/discourse/components/category-stats.js
@@ -2,18 +2,18 @@ import Component from "@glimmer/component";
 import Service, { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 
-export default class categoryStats extends Component {
+export default class CategoryStats extends Component {
   @service site
 
   @action
   getCategoryPostCount(categoryId){
     //check if Id exists (assumes that categoryId's cannot exceed current map of categories)
-    try{
-      if(site.categoriesById.size()<categoryId){
+    try {
+      if (site.categoriesById.size() < categoryId) {
         throw "category Id does not exist";
-      }catch(err){
-        console.log(err);
       }
+    }catch(err) {
+        console.log(err);
     }
     return site.categoriesById.get(categoryId).post_count;
   }
