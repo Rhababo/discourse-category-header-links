43ddefae68761b5520cbec1bd9bc914ada844b2c
Rhababo <Wesley.Adams@rhababo.com>
2023-09-29 10:38:00 -0500

    udpate category-stats.js

--- javascripts/discourse/components/category-stats.js
+++ javascripts/discourse/components/category-stats.js
@@ -5,22 +5,61 @@ import { action } from "@ember/object";
 export default class CategoryStats extends Component {
   @service site
 
-  postCount = 0;
-  categoryId = 4;
+  categoryIds = settings.category_ids.split("|");
 
-  @action
-  getCategoryPostCount(){
-    //check if Id exists (assumes that categoryId's cannot exceed current map of categories)
-    try {
-      if (this.site.categories.length < this.categoryId) {
-        throw "category Id does not exist";
+  //code assumes that (category_Id - 1) is equal to their position in the site.categories array
+  get shouldShow() {
+    for(let x; x < this.categoryIds.length; x++){
+      if(!this.categoryExists(this.categoryIds[x])){
+        return false;
       }
-    }catch(err) {
-        console.log(err);
     }
-    this.postCount = this.site.categories[this.categoryId-1].post_count;
+    return this.categoryIds.length > 0;
   }
 
+  categoryExists(categoryId){
+    if(!Number.isInteger(categoryId)){
+      return false;
+    }
+    return this.categoryIds.length < categoryId;
+  }
+
+  get links() {
+    return this.categoryIds.reduce((result, item) => {
+      let [
+        linkText = this.site.categories[item].
+        linkTitle,
+        linkHref,
+        device,
+        target = "",
+        keepOnScroll,
+        locale,
+      ] = item.split(",").map((s) => s.trim());
+
+      if (!linkText || (locale && document.documentElement.lang !== locale)) {
+        return result;
+      }
+
+      const linkClass = `${dasherize(linkText)}-custom-header-links`; // legacy name
+
+      const anchorAttributes = {
+        title: linkTitle,
+        href: linkHref,
+        target: target === "self" ? "" : "_blank",
+      };
+
+      result.push({
+        device: `headerLink--${device}`,
+        keepOnScroll: `headerLink--${keepOnScroll}`,
+        locale: `headerLink--${locale}`,
+        linkClass,
+        anchorAttributes,
+        linkText,
+      });
+
+      return result;
+    }, []);
+  }
 
 
 }
--- javascripts/discourse/components/custom-header-links.js
+++ javascripts/discourse/components/custom-header-links.js
@@ -3,7 +3,7 @@ import { dasherize } from "@ember/string";
 
 export default class CustomHeaderLinks extends Component {
   get shouldShow() {
-    return settings.Custom_header_links?.length > 0;
+    return settings.category_ids?.length > 0;
   }
 
   get links() {
